e21e3c38f1edcb80d3f1a060efad612655625e66
Tim Kurucz <tjkurucz11@gmail.com>
2022-03-16 12:12:19 -0400

    Added random computer choice

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 console.log("Hello world!")
+
+function computerPlay() {
+    let choice = Math.random();
+
+    if (choice < 0.34) {
+        return "rock"
+    } else if (choice < 0.67) {
+        return "paper"
+    } else {
+        return "scissor"
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == "rock") {
+
+    }
+}
+
+const playerSelection = "rock"
+const computerSelection = computerPlay();
+
+console.log(playRound(playerSelection, computerSelection));
