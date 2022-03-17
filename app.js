a4d7c99740330c4eeb69d5b9d5fdf5ba144ce15f
Tim Kurucz <tjkurucz11@gmail.com>
2022-03-17 10:07:27 -0400

    Added win and lose tracking over a 5 round game

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
-console.log("Hello world!")
-
 function computerPlay() {
     let choice = Math.random();
 
     if (choice < 0.34) {
-        return "rock"
+        return "rock";
     } else if (choice < 0.67) {
-        return "paper"
+        return "paper";
     } else {
-        return "scissor"
+        return "scissors";
     }
 }
 
@@ -16,44 +14,66 @@ function playRound(playerSelection, computerSelection) {
     switch (playerSelection){
         case "rock":
             if (computerSelection == "rock") {
-                console.log("It's a tie!")
+                console.log("It's a tie!");
+                return "tie";
             } else if (computerSelection == "paper") {
-                console.log("YOU LOSE!  Paper beats rock.")
+                console.log("YOU LOSE!  Paper beats rock.");
+                return "computerWin";
             } else {
-                console.log("YOU WIN! Rock beats scissors!")
+                console.log("YOU WIN! Rock beats scissors!");
+                return "playerWin";
             }
-            break;
+            //break;
         case "paper":
             if (computerSelection == "paper") {
-                console.log("It's a tie!")
+                console.log("It's a tie!");
+                return "tie";
             } else if (computerSelection == "scissors") {
-                console.log("YOU LOSE!  Scissors beat paper!")
+                console.log("YOU LOSE!  Scissors beat paper!");
+                return "computerWin";
             } else {
-                console.log("YOU WIN!  Paper beats rock!")
+                console.log("YOU WIN!  Paper beats rock!");
+                return "playerWin";
             }
-            break;
         case "scissors":
             if (computerSelection == "scissors") {
-                console.log("It's a tie!")
+                console.log("It's a tie!");
+                return "tie";
             } else if (computerSelection == "rock") {
-                console.log("YOU LOSE!  Rock beats scissors!")
+                console.log("YOU LOSE!  Rock beats scissors!");
+                return "computerWin";
             } else {
-                console.log("YOU WIN!  Scissors beat paper!")
+                console.log("YOU WIN!  Scissors beat paper!");
+                return "playerWin";
             }
-            break;
     }
 }
 
 function game() {
-    let playerWin = 0
-    let computerWin = 0
+    let playerWin = 0;
+    let computerWin = 0;
     for (let i = 0; i < 5; i++) {
         console.log("ROUND " + (i + 1));
-        const playerSelection = "rock"
-        const computerSelection = computerPlay()
+        const playerSelection = "scissors";
+        const computerSelection = computerPlay();
         console.log("Player selection: " + playerSelection);
         console.log("Computer selection: " + computerSelection);
-        console.log(playRound(playerSelection, computerSelection));
+        let winner = playRound(playerSelection, computerSelection);
+        if (winner == "playerWin") {
+            playerWin++;
+        } else if (winner == "computerWin") {
+            computerWin++;
+        }
+    }
+    console.log("Player wins: " + playerWin);
+    console.log("Computer wins: " + computerWin);
+
+    if (playerWin > computerWin) {
+        console.log("The overall winner is YOU!");
+    } else if (computerWin > playerWin) {
+        console.log("The overall winner is the COMPUTER!");
+    } else {
+        console.log("This game ends in a tie!");
     }
 }
 
